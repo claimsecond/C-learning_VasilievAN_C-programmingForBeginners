5e074ff048400ec801fd1fe0d12c9ed3454d70fd
claimsecond <claim.second@gmail.com>
2019-03-02 11:56:45 +0200

    ввод и вывод целого числа через оконную форму

--- Project1/CodeFile1.cs
+++ Project1/CodeFile1.cs
@@ -4,27 +4,27 @@ using System;
 
 class enteringInteger
 {
-    //static void Main()
-    //{
-    //    string res, txt;
-    //    int year = 2019, age, born;
-    //    res = Interaction.InputBox("В каком году Вы родились?", "Год рождения");
-    //    born = Int32.Parse(res);
-    //    age = year - born;
-    //    txt = "Тогда Вам " + age + " лет.";
-    //    MessageBox.Show(txt, "Возраст");
-    //}
-}
-
-class EnterNameAndSurname
-{
     static void Main()
     {
-        string name, surname, res;
-        name = Interaction.InputBox("Введите ваше имя", "Name");
-        surname = Interaction.InputBox("Введите вашу фамилию", "Surname");
-        res = "Вас зовут " + name + ' ' + surname;
-        MessageBox.Show(res, "Your name is ...");
+        string res, txt;
+        int year = 2019, age, born;
+        res = Interaction.InputBox("В каком году Вы родились?", "Год рождения");
+        born = Int32.Parse(res);
+        age = year - born;
+        txt = "Тогда Вам " + age + " лет.";
+        MessageBox.Show(txt, "Возраст");
     }
 }
+
+//class EnterNameAndSurname
+//{
+//    static void Main()
+//    {
+//        string name, surname, res;
+//        name = Interaction.InputBox("Введите ваше имя", "Name");
+//        surname = Interaction.InputBox("Введите вашу фамилию", "Surname");
+//        res = "Вас зовут " + name + ' ' + surname;
+//        MessageBox.Show(res, "Your name is ...");
+//    }
+//}
 
